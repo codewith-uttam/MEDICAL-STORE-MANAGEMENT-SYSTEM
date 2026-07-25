bf90fa45abe7b41231f6c30ebb23d7c5d379dd42
codewith-uttam <sankalp.uttam04@gmail.com>
2026-07-25 08:03:31 +0530

    Add Android Application support (PWA Manifest, Service Worker, Android Icons & Standalone App mode)

---
+++ sw.js
@@ -0,0 +1,49 @@
+/**
+ * Swastik Medical Store - Service Worker (Android PWA Support)
+ */
+
+const CACHE_NAME = 'swastik-med-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/css/styles.css',
+  '/js/app.js',
+  '/manifest.json'
+];
+
+// Install Event
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate Event
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch Event
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request).catch(() => {
+        return caches.match('/index.html');
+      });
+    })
+  );
+});
